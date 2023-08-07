48ca0c64988916a93543825913054349f9e9e300
Rodadev <>
2023-08-08 00:07:36 +0300

    XCode project; Authentification UI

---
+++ DSMessenger/App/DSMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSMessengerApp.swift
+//  DSMessenger
+//
+//  Created by Dmitry Sharygin on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DSMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DSMessenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,92 @@
+//
+//  LoginView.swift
+//  DSMessenger
+//
+//  Created by Dmitry Sharygin on 04.08.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                // logo image
+                Image("logoIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                // textFields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 16)
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 16)
+                }
+                
+                // forgot password
+                Button {
+                    print("Forgot password")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                // login button
+                Button {
+                    print("handle login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(10)
+                }
+                .padding(.vertical)
+                
+                Spacer()
+                
+                Divider()
+                
+                // sign up
+                NavigationLink {
+                    Text("Sign up")
+                } label: {
+                    HStack {
+                        Text("Don't have an account?")
+                        Text("Sign up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
